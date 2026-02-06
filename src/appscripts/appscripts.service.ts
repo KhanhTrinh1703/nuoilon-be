09aee1243823a558cdb61850678627f3beca6b9a
ANS-375-trinhdtk <153491049+ANS-375-trinhdtk@users.noreply.github.com>
2026-02-06 13:18:36 +0900

    feat: simplify transaction upsert logic and add average cost trigger

--- src/appscripts/appscripts.service.ts
+++ src/appscripts/appscripts.service.ts
@@ -1,21 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { format } from 'date-fns/format';
 import { isValid } from 'date-fns/isValid';
 import { parse } from 'date-fns/parse';
-import { Repository } from 'typeorm';
 import { ExcelTransactionRepository } from './repositories/excel-transaction.repository';
 import { UpsertExcelTransactionDto } from './dto/upsert-excel-transaction.dto';
-import { FundPrice } from '../database/entities/fund-price.entity';
 
 @Injectable()
 export class AppscriptsService {
-  private static readonly DEFAULT_FUND_NAME = 'E1VFVN30';
-
   constructor(
     private readonly excelTransactionRepository: ExcelTransactionRepository,
-    @InjectRepository(FundPrice)
-    private readonly fundPriceRepository: Repository<FundPrice>,
   ) {}
 
   async upsertExcelTransaction(dto: UpsertExcelTransactionDto) {
@@ -29,31 +22,7 @@ export class AppscriptsService {
       transactionId: dto.transactionId,
     };
 
-    const transaction =
-      await this.excelTransactionRepository.upsertTransaction(data);
-
-    const [totalCapital, totalCertificates] = await Promise.all([
-      this.excelTransactionRepository.getTotalCapital(),
-      this.excelTransactionRepository.getTotalCertificates(),
-    ]);
-
-    const averageCost =
-      totalCertificates > 0 ? totalCapital / totalCertificates : 0;
-
-    const fundPrice =
-      (await this.fundPriceRepository.findOne({
-        where: { name: AppscriptsService.DEFAULT_FUND_NAME },
-      })) ??
-      this.fundPriceRepository.create({
-        name: AppscriptsService.DEFAULT_FUND_NAME,
-        price: 0,
-      });
-
-    fundPrice.averageCost = averageCost;
-
-    await this.fundPriceRepository.save(fundPrice);
-
-    return transaction;
+    return await this.excelTransactionRepository.upsertTransaction(data);
   }
 
   private parseDateString(

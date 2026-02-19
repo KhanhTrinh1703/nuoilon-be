12756f273aa0e429353c8bc919eacfef7bb091cd
ANS-375-trinhdtk <153491049+ANS-375-trinhdtk@users.noreply.github.com>
2026-02-19 13:01:48 +0900

    feat: implement favicon retrieval with error handling and update greeting message format

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  StreamableFile,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { join } from 'path';
+import { createReadStream } from 'fs';
+import { access } from 'fs/promises';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +15,24 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get(['favicon.ico', 'favicon.png'])
+  async getFavicon(): Promise<StreamableFile> {
+    const imagePath = join(__dirname, '..', 'public', 'nuoilon.png');
+
+    // Check if file exists
+    try {
+      await access(imagePath);
+    } catch {
+      throw new NotFoundException('Report image not found');
+    }
+
+    const file = createReadStream(imagePath);
+
+    return new StreamableFile(file, {
+      type: 'image/png',
+      disposition: `inline; filename="nuoilon.png"`,
+    });
   }
 }
